55263d9713a1d8dcd0ae725096b7695c8726c81c
ngcongtunglam209 <2chidiepalz@proton.me>
2026-01-27 11:05:28 +0700

    yet another delete

--- src/commands/impl/slashes/admin/SetupCommand.ts
+++ src/commands/impl/slashes/admin/SetupCommand.ts
@@ -7,7 +7,6 @@ import {
 } from 'discord.js';
 import {Command} from '../../../Command';
 import GuildLog from '../../../../database/models/GuildLog.model';
-import TempVoiceChannel from '../../../../database/models/TempVoiceChannel.model';
 import ExtendedClient from '../../../../classes/ExtendedClient';
 import {StatusContainer} from '../../../../util/StatusContainer';
 
@@ -29,30 +28,6 @@ export default class SetupCommand extends Command {
                 .setDescription('Kênh log bạn muốn đặt')
                 .setRequired(true)
                 .addChannelTypes(ChannelType.GuildText),
-            ),
-        ),
-    );
-
-    this.data.addSubcommandGroup(group =>
-      group
-        .setName('tempvoice')
-        .setDescription('Cài đặt kênh trò chuyện tạm thời.')
-        .addSubcommand(subcommand =>
-          subcommand
-            .setName('create')
-            .setDescription('Tạo kênh trò chuyện tạm thời mới.')
-            .addStringOption(option =>
-              option
-                .setName('name')
-                .setDescription('Tên kênh tạm thời bạn muốn tạo')
-                .setRequired(true),
-            )
-            .addChannelOption(option =>
-              option
-                .setName('category')
-                .setDescription('Vị trí kênh bạn muốn đặt')
-                .addChannelTypes(ChannelType.GuildCategory)
-                .setRequired(false),
             ),
         ),
     );
@@ -104,76 +79,6 @@ export default class SetupCommand extends Command {
               const failedContainer = await StatusContainer.failed(
                 failedEmoji,
                 'Không thể lưu cài đặt. Vui lòng thử lại sau.',
-              );
-
-              await interaction.editReply({
-                components: [failedContainer],
-                flags: MessageFlags.IsComponentsV2,
-              });
-            }
-            break;
-          }
-        }
-        break;
-      }
-      case 'tempvoice': {
-        switch (subcommand) {
-          case 'create': {
-            const existingConfig = await TempVoiceChannel.findOne({
-              where: {
-                guildId: guild.id,
-              },
-            });
-
-            if (existingConfig) {
-              const failedContainer = await StatusContainer.failed(
-                failedEmoji,
-                'Máy chủ này đã được thiết lập kênh voice tạm thời.',
-              );
-
-              await interaction.editReply({
-                components: [failedContainer],
-                flags: MessageFlags.IsComponentsV2,
-              });
-              return;
-            }
-
-            const channelName = interaction.options.getString('name', true);
-            const channelCategory =
-              interaction.options.getChannel<ChannelType.GuildCategory>(
-                'category',
-                false,
-                [ChannelType.GuildCategory],
-              );
-
-            try {
-              const newTempVoiceChannel =
-                await guild.channels.create<ChannelType.GuildVoice>({
-                  name: channelName,
-                  type: ChannelType.GuildVoice,
-                  parent: channelCategory,
-                });
-
-              const newTempVoiceChannelDB = new TempVoiceChannel({
-                guildId: guild.id,
-                channelId: newTempVoiceChannel.id,
-              });
-
-              await newTempVoiceChannelDB.save();
-
-              const successContainer = await StatusContainer.success(
-                successEmoji,
-                `## ${successEmoji} Đã tạo kênh voice tạm thời ${channelMention(newTempVoiceChannel.id)}.`,
-              );
-
-              await interaction.editReply({
-                components: [successContainer],
-                flags: MessageFlags.IsComponentsV2,
-              });
-            } catch {
-              const failedContainer = await StatusContainer.failed(
-                failedEmoji,
-                `## ${failedEmoji} Đã xảy ra lỗi khi tạo kênh voice tạm thời. Vui lòng thử lại sau.`,
               );
 
               await interaction.editReply({
